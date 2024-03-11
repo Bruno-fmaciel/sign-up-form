3e2e5d7fa45266fd2f24ab547aaf9fac5d8b71d4
Bruno-fmaciel <bfma@cesar.school>
2024-03-11 18:21:12 -0300

    Update HTML and CSS, create js

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach(input => {
+    input.addEventListener("invalid", event => {
+        input.classList.add("error");
+    }, false)
+})
